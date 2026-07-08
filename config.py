d4687142566761d285e289461ee93df09b3953b7
Jaeden West <101752952+JaedenWest@users.noreply.github.com>
2026-07-08 00:09:16 -0500

    Create config.py for path configurations
    Add configuration for file paths including downloads and OneDrive folders.

---
+++ config.py
@@ -0,0 +1,11 @@
+#stores path 
+from pathlib import Path# represents the files or folders
+
+#Downloads location
+DOWNLOADS_FOLDER = Path.home() / "Downloads"
+
+# Shared OneDrive folders 
+JHA_Folder = Path("put your path)
+WO_Folder = Path("put your path)
+Phase_2_WO = Path("put your path)
+
